80e30a0ce34625d40440b165642eb64f7c3df5e2
a-maged <a.maged99@outlook.com>
2019-01-04 17:47:15 +0200

    reset state on logout

--- resources/assets/js/redux/reducers/rootReducer.js
+++ resources/assets/js/redux/reducers/rootReducer.js
@@ -1,17 +1,26 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
+import * as actionTypes from '../actions/actionTypes';
 import authReducer from './authReducer';
 import uiReducer from './uiReducer';
 import courseReducer from './courseReducer';
 import routingReducer from './routingReducer';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
 	auth: authReducer,
 	ui: uiReducer,
 	course: courseReducer,
 	form: formReducer,
 	routing: routingReducer
 });
+
+const rootReducer = (state, action) => {
+	// resets all state when logging out
+	if (action.type === actionTypes.AUTH_LOGOUT_REQUEST) {
+		state = undefined;
+	}
+	return appReducer(state, action);
+};
 
 export default rootReducer;
